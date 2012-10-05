70fc1dac3ab958e84b492dab341c093e7328037b
Jehan Tremback <jehan.tremback@gmail.com>
2012-10-04 19:24:44 -0700

    much progress

--- source/javascripts/controllers.js
+++ source/javascripts/controllers.js
@@ -33,6 +33,56 @@ function BucketList($scope) {
   ];
 }
 
+function HistoryList($scope) {
+  $scope.annotations = [
+    {
+      username: "xXbRiAnSgIrLXx",
+      time: "about 3 days ago",
+      page: "China cancels waste project after protests turn violent | Reuters",
+      link: "http://www.reuters.com/article/2012/07/28/us-china-environment-protest-idUSBRE86R02Y20120728",
+      domain: "reuters.com",
+      excerpt: "Demonstrators seized bottles of liquor and wine from the offices along with cartons of cigarettes, items which Chinese officials frequently receive as bribes. A photograph posted on Sina Weibo, the main Chinese microblogging service, showed some of the items displayed outside the government building.",
+      text: "Chinese officials sound really easy to bribe."
+    },
+    {
+      username: "FutureFry",
+      time: "about 21 hours ago",
+      page: "Microsoft Files Motion in Apple v. Samsung to Hide Patent License Agreement Terms ~pj",
+      link: "http://groklaw.net/article.php?story=20120727084323510",
+      domain: "groklaw.net",
+      excerpt: "I seriously want to see those license terms, and I'd go so far as to suggest that the public has a right to know what those terms are, particularly future victims of Microsoft's patent strategy, and I know you want to know this too, because we've all heard the rumors that Microsoft licenses on very, very low royalties, just to be able to say to the world that Android/Linux folks are paying Microsoft for its patents. I'd love to know if that is true. Plus, if Microsoft paid for Samsung's FRAND patents the fee Samsung is asking Apple for, on what basis would Microsoft argue in its litigation against Motorola, that FRAND patents should be paid for at a greatly reduced royalty?",    
+      text: "Seems to me that the article singles out Microsoft because it puts this motion in the context of Microsoft making secret patent deals with Android manufacturers:"
+    },
+    {
+      username: "brian_buggers",
+      time: "1 day ago",
+      page: "How Microsoft Lost Its Mojo: Steve Ballmer and Corporate America’s Most Spectacular Decline | Business | Vanity Fair",
+      link: "http://m.vanityfair.com/business/2012/08/microsoft-lost-mojo-steve-ballmer",
+      domain: "m.vanityfair.com",
+      excerpt: "In December 2000, Microsoft had a market capitalization of $510 billion, making it the world’s most valuable company. As of June it is No. 3, with a market cap of $249 billion. In December 2000, Apple had a market cap of $4.8 billion and didn’t even make the list. As of this June it is No. 1 in the world, with a market cap of $541 billion.",
+      text: "In other words, Apple is currently about where Microsoft was when they started paying dividends a little more than a decade ago...i.e. The point where they went from a growth company to a the sort of 'blue chip' held by index funds. The past decade has been spent securing their place in enterprise - their core market and one in which Apple, Google, and Facebook offer little competition. With loads of cash, a conucopia of brilliant personnel and Gates and Ballmer as the two largest shareholders, the whims of Wall Street bloggers don't have much effect."
+    },
+    {
+      username: "Mb'Skepteko",
+      time: "about 6 hours ago",
+      page: "Japanese equestrian defies Father Time as the oldest competitor at London Olympics - Yahoo! Sports",
+      link: "http://sports.yahoo.com/news/olympics--japanese-equestrian-defies-father-time-as-oldest-competitor-at-london-olympics.html",
+      domain: "sports.yahoo.com",
+      excerpt: "I have not seen my wife, Motoko, for more than a year",
+      text: "The key to a long-lasting marriage"
+    },
+    {
+      username: "P1MP_$TEVE",
+      time: "about 1 hour ago",
+      page: "German renewables output hits record high in H1 | Reuters",
+      link: "http://www.reuters.com/article/2012/07/26/germany-renewables-idUSL6E8IQIA720120726",
+      domain: "reuters.com",
+      excerpt: "Renewables now account for 25 percent of energy production, up from 21 percent last year, the country's energy industry association (BDEW) said in a statement that reinforced Germany's position as a leader in green technology.",
+      text: "In related news, German electricity is 30% more expensive than French, and 300% more expensive than American."
+    }
+  ];
+}
+
 function DetailList($scope) {
   $scope.annotations = [
     {
--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,24 +1,31 @@
 //SLIDING DOORS
 $(document).ready(function(){
 
-  $(".hyp-summary").click(function(){
+  $(".hyp-summary").click(function(event){
     $("#wrapper2").addClass("out");
     $("#wrapper1").addClass("compressed");
+    $("#wrapper3").addClass("compressed");
     event.stopPropagation();    
   });
 
-  $(document).click(function() {
+  $(".history").click(function(event){
+    $("#wrapper3").addClass("shown");
+    event.stopPropagation();    
+  });
+
+  $(document).click(function(event) {
     $("#wrapper2").removeClass("out");
     $("#wrapper1").removeClass("compressed");
+    $("#wrapper3").removeClass("compressed");
     $(".hyp-heatmap-tab").removeClass("flip");
   });
 
   $("#wrapper2").click(function(event) {
-      alert('clicked inside');
-      event.stopPropagation();
+    alert('clicked inside');
+    event.stopPropagation();
   });
 //FLIPPER
-  $(".hyp-heatmap-tab.side").click(function(){
+  $(".hyp-heatmap-tab.side").click(function(event){
     event.stopPropagation();
     $(this).addClass("flip");
   });
